c97f6cd764f976f91f2fb0bc88a6baae7568a8cf
cameverett <cameronjeverett@gmail.com>
2016-11-19 10:48:11 -0500

    navigation from HomeActivity functional

--- app/src/main/java/com/makify/makify/HomeActivity.java
+++ app/src/main/java/com/makify/makify/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.makify.makify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
+
+    public void goToBlankActivity(View view) {
+        Intent intent = new Intent(this, BlankActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToImportActivity(View view) {
+        Intent intent = new Intent(this, ImportActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLoadActivity(View view) {
+        Intent intent = new Intent(this, LoadActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToTemplateActivity(View view) {
+        Intent intent = new Intent(this, TemplateActivity.class);
+        startActivity(intent);
+    }
+
 }
